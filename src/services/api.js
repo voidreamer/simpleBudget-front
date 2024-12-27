7e2a5bf6534c16a1091c7409b5fd1ed17e659b48
Alejandro <voidreamer@gmail.com>
2024-12-27 15:26:43 -0500

    fix url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 // src/services/api.js
-//const API_BASE_URL = 'https://simplebudget-app.onrender.com/api';
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'https://simplebudget-app.onrender.com/api';
 
 export const budgetApi = {
   async fetchBudgetData(year, month) {
